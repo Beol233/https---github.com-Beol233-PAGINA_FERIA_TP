645ef0b10f2c6fd71a71ca695289fbab09c8db45
josue penroz <josue.penroz.maldonado@alumnos.sip.cl>
2026-07-30 17:49:14 -0400

    add

--- biblioteca_mvp/appy.py
+++
@@ -1,9 +0,0 @@
-from flask import Flask, render_template, request, redirect, url_for, session, flash
-import pymysql
-from datetime import date, timedelta
-from functools import wraps
-
-app = Flask(__name__)
-
-
-app.secret_key = "biblioteca_secreta_cambiar_en_produccion"
