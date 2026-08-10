d5dd953195940015d33e3cb8f4279cb1993e0a4a
Carlos <carlos.bquedano.miranda@alumnos.sip.cl>
2026-08-10 12:59:50 -0400

    canbios_usuario

--- usuario.py
+++ usuario.py
@@ -5,4 +5,10 @@ class Usuario:
     def __init__(self,data ):
         self.id = data['id']
         self.nombre = data['nombre']
-        
+        self.apellido = data['apellido']
+        self.correo = data['correo']
+        self.password = data['password']
+        self.created_at = data['created_at']
+        self.updated_at = data['updated_at']
+        self.rol_id = data['rol_id']
+
