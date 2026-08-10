e651a2b17e35b4aa6217bb2af4135a3866f25cdb
Beol233 <benjamin.olguin.hernandez@alumnos.sip.cl>
2026-08-10 12:55:07 -0400

    10-08

---
+++ prestamo.py
@@ -0,0 +1,22 @@
+from mysqlconnection import connectToMySQL
+
+class Prestamo:
+   def __init__( self , data ):
+       self.id = data['id']
+       self.usuario_id = data['usuario_id']
+       self.libro_id = data['libro_id']
+       self.created_at = data['created_at']
+       self.updated_at = data['updated_at']
+       self.fecha_dev_esperada = data['fecha_dev_esperada']
+
+   @classmethod                                             
+   def get_all(cls):
+       query = "SELECT * FROM prestamo;"
+
+       resultados = connectToMySQL('primera_flask').query_db(query)
+
+       prestamo = []
+
+       for prestamo in resultados:
+           prestamo.append( cls(prestamo) )
+       return prestamo
