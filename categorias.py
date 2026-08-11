74c15115eaa1ed20a05a0892c4b6de733fc632b4
cris02932 <cristian.reyes.pinochet@alumnos.sip.cl>
2026-08-11 08:42:09 -0400

    clase_categoria

---
+++ categorias.py
@@ -0,0 +1,23 @@
+from mysqlconnection import connectToMySQL
+
+class Categorias:
+
+    def __init__(self,data ):
+        self.id = data['id']
+        self.nombre = data['nombre']
+        self.descripcion = data['descripcion']
+        self.updated_at = data['updated_at']
+        self.created_at = data['created_at']
+
+@classmethod
+def get_all(cls):
+    query = "SELECT * FROM categorias;"
+
+    resultados = connectToMySQL('biblioteca_db').query_db(query)
+
+    Categorias = []
+
+    for categoria in resultados:
+        Categorias.append(cls(categoria))
+
+    return Categorias
