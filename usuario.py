773beeab7d9503b9742f8df8b48dfac45921e379
Carlos <carlos.bquedano.miranda@alumnos.sip.cl>
2026-08-11 09:16:09 -0400

    usuario_py

--- usuario.py
+++ usuario.py
@@ -12,3 +12,16 @@ class Usuario:
         self.updated_at = data['updated_at']
         self.rol_id = data['rol_id']
 
+def get_all(cls):
+       query = "SELECT * FROM Usuario;"
+
+       # Llamamos a función connectToMySQL con el esquema al que te diriges
+       resultados = connectToMySQL('biblioteca_db').query_db(query)
+
+       # Creamos una lista vacía para agregar nuestras instancias de mascota
+       usuario = []
+
+       # Iteramos sobre los resultados de la base de datos y crear instancias de mascota con cls
+       for usuario in resultados:
+           usuario.append( cls(usuario) )
+       return usuario
