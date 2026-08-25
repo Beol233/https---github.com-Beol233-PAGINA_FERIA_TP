ce409f932c21ae0777fea2b8b8b9cf5c4d44ae5f
Josuepenroz <josue.penroz.maldonado@alumnos.sip.cl>
2026-08-25 12:57:23 -0400

    perfilusuario

--- app.py
+++ app.py
@@ -378,6 +378,9 @@ def escaner():
 def prestamos():
     return render_template("prestamo.html")
 
+@app.route("/perfil")
+def perfil():
+    return render_template("perfil.html")
 
 if __name__ == "__main__":
     app.run(debug=True)
